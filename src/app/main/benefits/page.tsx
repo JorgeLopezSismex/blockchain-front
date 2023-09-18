44c064e087440ad66ad6ce8b8e18d12cc57a40a1
tebazile <141357064+tebazile@users.noreply.github.com>
2023-09-18 14:10:49 -0600

    fix: benefits cards added

--- src/app/main/benefits/page.tsx
+++ src/app/main/benefits/page.tsx
@@ -2,6 +2,8 @@
 //Ventana About us (*/ω＼*)
 import { Col, Container, Row } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
+import Card from 'react-bootstrap/Card';
+import BenefitsCard from "../../components/main/BenefitsCard";
 
 export default function AboutUs() {
   return (
@@ -12,10 +14,13 @@ export default function AboutUs() {
             <Col xs={12} lg={7} className="px-5 py-3 align-items-center" style={{display:'flex'}}>
               <div className="wrapper align-left">
                 <h2 style={{fontSize:'50px'}}>Por que deberias usar Blockchain</h2>
-                <p>
-                Nuestros expertos en blockchain, educación y 
-                tecnología trabajan incansablemente para 
-                ofrecerte una solución de vanguardia. 
+                <p style={{fontSize:'25px'}}>
+                Blockchain está diseñado para proporcionarte 
+                una experiencia excepcional en la generación 
+                y verificación de certificados digitales. 
+                Descubre los beneficios de utilizar nuestros 
+                servicios:
+                 
                 </p>
               </div>
             </Col>
@@ -23,39 +28,50 @@ export default function AboutUs() {
             <Col xs={12} lg={5} className="px-3 py-3">
               <Image
                 style={{ width: "100%" }}
-                src="https://static.vecteezy.com/system/resources/previews/005/915/627/non_2x/hand-drawn-doodle-set-of-blockchain-theme-items-round-composition-sketch-style-cryptocurrency-electronic-commerce-concept-illustration-vector.jpg"
+                src="https://create.vista.com/s3-static/create/uploads/2022/09/make-participation-certificate-online.webp"
               ></Image>
             </Col>
 
         </Row>
+
         <Row>
-          <Col>
-            <h3>Seguridad inquebrantable</h3>
-            <p>
-              La tecnología blockchain asegura que tus certificados 
-              sean inmutables y a prueba de falsificaciones.
-            </p>
+          <img
+            src="https://53.fs1.hubspotusercontent-na1.net/hubfs/53/WSG%20(2).png"
+            alt="WebPage"
+            className="img-fluid mx-3"
+            style={{width:'100%', height:'auto'}}
+          />
+          
+          <Col className='my-3' xs={12} md={6} lg={3}>
+            <BenefitsCard
+                titulo="Seguridad inquebrantable"
+                contenido="La tecnología blockchain asegura que tus certificados 
+                sean inmutables y a prueba de falsificaciones."
+            />
           </Col>
-          <Col>
-            <h3>Verificación Instantánea</h3>
-            <p>
-              Valida la autenticidad de tus certificados en 
-              segundos, en cualquier lugar y en cualquier momento.
-            </p>
+          
+          <Col className='my-3' xs={12} md={6} lg={3}>
+            <BenefitsCard
+                titulo="Verificación Instantánea"
+                contenido="Valida la autenticidad de tus certificados en 
+                segundos, en cualquier lugar y en cualquier momento."
+            />
           </Col>
-          <Col>
-            <h3>Sencillez en la Emisión</h3>
-            <p>
-              Emite certificados de forma rápida y eficiente, 
-              reduciendo la carga administrativa.
-            </p>
+
+          <Col className='my-3' xs={12} md={6} lg={3}>
+            <BenefitsCard
+                titulo="Sencillez en la Emisión"
+                contenido="Emite certificados de forma rápida y eficiente, 
+                reduciendo la carga administrativa."
+            />
           </Col>
-          <Col>
-            <h3>Transparencia Total</h3>
-            <p>
-              Todas las partes interesadas pueden rastrear el 
-              historial de emisión y verificación de certificados.
-            </p>
+
+          <Col className='my-3' xs={12} md={6} lg={3}>
+            <BenefitsCard
+                titulo="Transparencia Total"
+                contenido="Todas las partes interesadas pueden rastrear el 
+                historial de emisión y verificación de certificados."
+            />
           </Col>
         </Row>
       </Container>
