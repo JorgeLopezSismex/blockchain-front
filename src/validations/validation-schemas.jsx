55f329f30b2db3f6df514eb707d882b5077e9c17
Jorge Lopez <jalopez@sismex.com>
2023-09-22 15:23:37 -0600

    fix: added fetch to sign-in and sign-up pages

--- src/validations/validation-schemas.jsx
+++ src/validations/validation-schemas.jsx
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import { apiFetch } from "@/helpers/api-fetch";
 
 export const signInSchema = yup.object().shape({
   email: yup
@@ -20,12 +21,27 @@ export const signUpSchema = yup.object().shape({
     .required("Este campo es obligatorio.")
     .email("Por favor, escribe una dirección de correo válida.")
     .min(5, "Por favor, no escribas menos de 5 caracteres.")
-    .max(150, "Por favor, no escribas más de 150 caracteres."),
+    .max(100, "Por favor, no escribas más de 150 caracteres.")
+    .test(
+      "Esta dirección de correo es válida.",
+      "Esta dirección de correo ya esta en uso.",
+      async (value, values) => {
+        const res = await apiFetch("authorization/registered-email", "POST", {
+          email: value,
+        });
+
+        return res.success ? true : false;
+      }
+    ),
   password: yup
     .string()
     .required("Este campo es obligatorio.")
     .min(8, "Por favor, no escribas menos de 8 caracteres.")
-    .max(16, "Por favor, no escribas más de 16 caracteres."),
+    .max(16, "Por favor, no escribas más de 50 caracteres.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!*]).{8,}$/,
+      "Por favor, la contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial."
+    ),
   repeatPassword: yup
     .string()
     .required("Este campo es obligatorio.")
