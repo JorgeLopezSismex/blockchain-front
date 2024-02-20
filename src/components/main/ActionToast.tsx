fa4423630c51f149dd7ec80111bc64a50e32f5a8
Jorge Lopez <jalopez@sismex.com>
2024-02-20 16:59:27 -0600

    fix: fixed toast alert to shows it always on bottom left even when page is scrolled

--- src/components/main/ActionToast.tsx
+++ src/components/main/ActionToast.tsx
@@ -7,21 +7,29 @@ export default function ActionToast({
   title,
   message,
   onClose,
-  delay
-}:any) {
+  delay,
+}: any) {
   return (
-    <ToastContainer
-      className="p-3"
-      style={{ zIndex: 1 }}
-      position="bottom-start"
-    >
-      <Toast bg={variant} show={show} onClose={onClose} delay={delay} autohide>
-        <Toast.Header>
-          {/* <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" /> */}
-          <strong className="me-auto">{title}</strong>
-        </Toast.Header>
-        <Toast.Body style={{ color: "white" }}>{message}</Toast.Body>
-      </Toast>
-    </ToastContainer>
+    <div style={{ position: "fixed", bottom: 0, left: 0, width: '100%', zIndex: 9999 }}>
+      <ToastContainer
+        className="p-3"
+        style={{ zIndex: 1 }}
+        position="bottom-start"
+      >
+        <Toast
+          bg={variant}
+          show={show}
+          onClose={onClose}
+          delay={delay}
+          autohide
+        >
+          <Toast.Header>
+            {/* <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" /> */}
+            <strong className="me-auto">{title}</strong>
+          </Toast.Header>
+          <Toast.Body style={{ color: "white" }}>{message}</Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </div>
   );
 }
